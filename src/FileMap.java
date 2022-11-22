7dc2d67689ec9031dd3f60b536424a12ccf6905b
khalil reghaye <mohamed.khalil.rerhrhaye@umontreal.ca>
2022-11-22 15:27:10 -0500

    Initial commit

---
+++ src/FileMap.java
@@ -0,0 +1,31 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class FileMap<K,V> implements Map<K,V> {
+    @Override
+    public int size() { }
+    @Override
+    public boolean isEmpty() { }
+    @Override
+    public boolean containsKey(Object key) { }
+    @Override
+    public boolean containsValue(Object value) { }
+    @Override
+    public V get(Object key) {}
+    @Override
+    public V put(K key, V value) {}
+    @Override
+    public V remove(Object key) {}
+    @Override
+    public void putAll(Map<? extends K,? extends V> m) {}
+    @Override
+    public void clear() {}
+    @Override
+    public Set<K> keySet() {}
+    @Override
+    public Collection<V> values() {}
+    @Override
+    public Set<Map.Entry<K,V>> entrySet() {}
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    /**
+     * Driver class method.
+     * @param args Not used.
+     * @throws Exception Throws file exception.
+     */
+    public static void main(String[] args) throws Exception {
+    }
+}
